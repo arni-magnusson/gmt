cbb2beff6293b17f14eda4c0efe350e55d61edeb
Arni Magnusson <arnima@hafro.is>
2017-06-19 18:52:25 +0200

    Source and dump data

--- data/demo.bar.R
+++ data/demo.bar.R
@@ -1,4 +1,4 @@
-`demo.bar` <-
+demo.bar <-
 structure(list(Lon = c(-12.5, -12.5, -12.5), Lat = c(58L, 62L, 
 66L), Width = c(0.5, 0.5, 0.5), Height = c(2L, 2L, 2L)), .Names = c("Lon", 
 "Lat", "Width", "Height"), class = "data.frame", row.names = c(NA, 
