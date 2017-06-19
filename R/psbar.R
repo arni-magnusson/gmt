24fbb54749f4130ad45971904af887a49ac201b1
Arni Magnusson <arnima@hafro.is>
2017-06-19 12:36:56 +0200

    Convert latin-1 to ASCII

--- R/psbar.R
+++ R/psbar.R
@@ -18,20 +18,20 @@ psbar <- function(x, cmd="-J -R -W1p -G180 -O -K", file=getOption("gmt.file"), r
   lon <- deg2num(x.matrix[,1])   # longitude at bar center
   lat <- deg2num(x.matrix[,2])   # latitude at bar base
   w   <- x.matrix[,3]            # bar width in degrees
-  h   <- x.matrix[,4]            # bar height in user units (1 degree high at ref°N/S)
+  h   <- x.matrix[,4]            # bar height in user units (1 degree high at ref latitude)
   n   <- nrow(x.matrix)          # number of bars
 
   ##   Y = log(tan(pi/4+LAT*pi/360))
   ##   LAT = (atan(exp(Y))-pi/4) / (pi/360)
   ##   Y'(LAT) = (pi/360) / (tan(pi/4+LAT*pi/360)*cos(pi/4+LAT*pi/360)^2)
 
-  ##   LAT      0°     10°    20°    30°    40°    50°    60°    70°    80°    90°
+  ##   LAT      0      10     20     30     40     50     60     70     80     90
   ##   Y        0.000  0.175  0.356  0.549  0.763  1.011  1.317  1.735  2.436  37.332
   ##   Y'(LAT)  0.017  0.018  0.019  0.020  0.023  0.027  0.035  0.051  0.101  Inf
 
   ## 2 Create bar.frame containing coordinates
   dYlat <- (pi/360) / (tan(pi/4+lat*pi/360)*cos(pi/4+lat*pi/360)^2)  # derivative of Mercator Y at each latitude
-  dYref <- (pi/360) / (tan(pi/4+ref*pi/360)*cos(pi/4+ref*pi/360)^2)  # derivative of Mercator Y at ref°N/S
+  dYref <- (pi/360) / (tan(pi/4+ref*pi/360)*cos(pi/4+ref*pi/360)^2)  # derivative of Mercator Y at ref latitude
   h.deg <- h * round(dYref/dYlat, digits)                            # bar height in degrees
   left      <- lon - w/2                                             # left bar edge
   right     <- lon + w/2                                             # right bar edge
