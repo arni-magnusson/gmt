02d75ddf32b9029529e4f71551b800b93ccc4e14
Arni Magnusson <thisisarni@gmail.com>
2022-06-06 03:06:07 +1100

    Use grepl() instead of custom function

--- R/r2gmt.R
+++ R/r2gmt.R
@@ -1,16 +1,14 @@
 r2gmt <- function(x, datafile, append=FALSE)
 {
-  is.digit <- function(x) suppressWarnings(!is.na(as.numeric(x)))
-
   ## 1  Import x if it's a file
   if(is.character(x) && file.exists(x))
   {
     ## Assume 1 line header if first non-whitespace char ...
     char <- substring(scan(x,what="",n=1,quiet=TRUE), 1, 1)
     ## ... is not comment, minus, point, or number
-    header <- all(char!="#", char!="-", char!=".", !is.digit(char))
+    header <- !grepl("#|-|\\.|[0-9]", char)
     ## Assume comma separator if first line has comma
-    sep <- if(length(grep(",",readLines(x,n=1))) > 1) "," else ""
+    sep <- if(grepl(",", readLines(x,n=1))) "," else ""
     x <- read.table(x, sep=sep, header=header)
   }
 
